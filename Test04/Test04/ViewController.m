02f0a1c1ffd345eac497273553541d4122f782ce
GYX364 <18563981716@163.com>
2016-03-25 14:51:51 +0800

    1

--- Test04/Test04/ViewController.m
+++ Test04/Test04/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"123");
+    
+        NSLog(@"123");
+        NSLog(@"123");
+        NSLog(@"123");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
