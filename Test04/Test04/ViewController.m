b48089cbc7d02e6726f728ebcff02b2ff7ce5d7e
GYX364 <18563981716@163.com>
2016-03-25 14:57:02 +0800

    3

--- Test04/Test04/ViewController.m
+++ Test04/Test04/ViewController.m
@@ -21,6 +21,9 @@
         NSLog(@"123");
         NSLog(@"123");
         NSLog(@"123");
+            NSLog(@"123");
+            NSLog(@"123");
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
